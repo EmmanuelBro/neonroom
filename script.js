b4759569e64866d6f6e5213e265929731372814d
Emmanuel Briseño <emmanuel-bri@hotmail.com>
2024-10-13 14:46:23 -0600

    fifht commit

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Menú hamburguesa (abrir/cerrar)
+// Lógica para abrir/cerrar el menú hamburguesa y transformar el ícono en una "X"
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 
+// Escuchar el clic en el ícono de hamburguesa
 burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle('toggle'); // Añade esta línea para animación y cambiar el estado
+    nav.classList.toggle('nav-active'); // Desplegar el menú
+    burger.classList.toggle('toggle'); // Cambiar el ícono de hamburguesa a tache
 });
