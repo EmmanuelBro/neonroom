5ff784d7ed6db98b16ef64a2cfd5788ae12bd5ca
Emmanuel Briseño <emmanuel-bri@hotmail.com>
2024-10-13 06:16:54 -0600

    fourt commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Menú responsivo
-
+// Menú hamburguesa (abrir/cerrar)
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle'); // Añade esta línea para animación y cambiar el estado
 });
