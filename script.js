2764cadac0b2ba9b0f6ec4f4c4fa8c37334165d6
Emmanuel Briseño <emmanuel-bri@hotmail.com>
2024-10-14 03:20:57 -0600

    reglamentos v2

--- script.js
+++ script.js
@@ -19,30 +19,3 @@ function adjustMainPadding() {
 window.addEventListener('load', adjustMainPadding);
 window.addEventListener('resize', adjustMainPadding);
 
-//Audio scripts
-// Obtener referencias al audio y al botón
-var myAudio = document.getElementById("myAudio");
-var muteButton = document.getElementById("muteButton");
-
-// Iniciar el audio automáticamente al cargar la página
-window.onload = function() {
-    myAudio.play().catch(function() {
-        // Algunos navegadores requieren interacción del usuario para reproducir audio
-        console.log("El usuario necesita interactuar con la página para iniciar el audio.");
-    });
-}
-
-// Variable para llevar el control del estado del audio
-var isPlaying = true;
-
-// Función para pausar/reanudar el audio
-muteButton.onclick = function() {
-    if (isPlaying) {
-        myAudio.pause();
-        muteButton.src = "images/volume-off.png";
-    } else {
-        myAudio.play();
-        muteButton.src = "images/volume-on.png";
-    }
-    isPlaying = !isPlaying;
-}
